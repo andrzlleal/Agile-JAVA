0a75b2626978509aa932250dd27d6de89e3fe855
andrzlleal <mirelyrusher@gmail.com>
2024-04-11 13:25:39 -0300

    Foram feitas alterações no código por motivos de erro de sintaxe

--- src/test/java/IOUtilTest.java
+++ src/test/java/IOUtilTest.java
@@ -1,3 +1,6 @@
+import org.junit.Test;
+import util.TestUtil;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -8,21 +11,25 @@ public class IOUtilTest {
     static final String FILENAME1 = "IOUtilTest1.txt";
     static final String FILENAME2 = "IOUtilTest2.txt";
 
+    @Test
     public void testDeleteSingleFile() throws IOException {
         create(FILENAME1);
 
         assertTrue(IOUtil.delete(FILENAME1));
         TestUtil.assertGone(FILENAME1);
     }
+    @Test
     public void testDeleteMultipleFiles() throws IOException {
         create(FILENAME1, FILENAME2);
         assertTrue(IOUtil.delete(FILENAME1, FILENAME2));
         TestUtil.assertGone(FILENAME1, FILENAME2);
     }
+    @Test
     public void testDeleteNoFile() throws IOException {
         TestUtil.delete(FILENAME1);
         assertFalse(IOUtil.delete(FILENAME1));
     }
+    @Test
     public void testDeletePartiallySuccessful() throws IOException {
         create(FILENAME1);
         TestUtil.delete(FILENAME2);
