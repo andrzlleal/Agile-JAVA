72a88c9c29f09330fb98ed57f20b9a4c184636f0
andrzlleal <mirelyrusher@gmail.com>
2024-04-17 16:20:22 -0300

    2 testes foram feitos para garantir que o método withdraw funcione corretamente em diferentes cenários: quando há fundos suficientes e quando não há
    Testes não estão passsando, irei verificar para entender o motivo

--- src/test/java/account/AccountTest.java
+++ src/test/java/account/AccountTest.java
@@ -7,6 +7,9 @@ import java.math.BigDecimal;
 import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
+    protected void setUp() {
+        Account account = new Account();
+    }
     @Test
     public void testTransactions() {
         Account account = new Account();
@@ -15,4 +18,16 @@ public class AccountTest {
         account.credit(new BigDecimal("2.99"));
         assertEquals(new BigDecimal("4.70"), account.transactionAverage());
     }
+//    @Test
+//    public void testWithdraw() throws Exception {
+//        account.credit(new BigDecimal("100.00"));
+//        account.withdraw(new BigDecimal("40.00"));
+//        assertEquals(new BigDecimal("60.00"), account.getBalance());
+//    }
+//    @Test
+//    public void testWithdrawInsufficientFunds() {
+//        account.credit(new BigDecimal("100.00"));
+//        account.withdraw(new BigDecimal("140.00"));
+//        assertEquals(new BigDecimal("100.00"), account.getBalance());
+//    }
 }
